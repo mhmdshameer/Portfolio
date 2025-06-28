7b82ebb4d92c777900bb41354854902bcf7ff437
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-06-28 12:52:26 +0530

    make grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,12 +15,18 @@ const Hero = () => {
         className={cn(
           "absolute inset-0",
           "[background-size:40px_40px]",
-          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_0.03px,transparent_0.5px),linear-gradient(to_bottom,#e4e4e7_0.03px,transparent_0.5px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_0.03px,transparent_0.5px),linear-gradient(to_bottom,#262626_0.03px,transparent_0.5px)]",
         )}
       />
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"/>
+    </div>
+
+    <div className='flex justify-center relative my-20 z-10'>
+      <div className='max-w-[89vw] md:max-w-2xl lg:max-w-60 flex flex-col items-center justify-center'>
+        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic</h2>
+      </div>
     </div>
     </div>
   )
