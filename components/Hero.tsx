c4f392e05ee11fe11696168242a751ad3b09cbc0
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-06-29 19:46:45 +0530

    background change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,16 +16,23 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center, transparent_20%,black)]"/>
-
-      </div>
-
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-60 flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic
-          </h2>
+      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_0.03px,transparent_0.5px),linear-gradient(to_bottom,#e4e4e7_0.03px,transparent_0.5px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_0.03px,transparent_0.5px),linear-gradient(to_bottom,#262626_0.03px,transparent_0.5px)]"
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-60 flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Dynamic Web Magic
+            </h2>
+          </div>
         </div>
       </div>
     </div>
