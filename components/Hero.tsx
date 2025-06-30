501f633eabe95b0c17df496871283083f6779b61
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-06-30 19:54:50 +0530

    Text Geerator edit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,13 @@ const Hero = () => {
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
               Dynamic Web Magic
             </h2>
-            <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experience"/>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Transforming Concepts into Seamless Experience"
+            />
+            <p className="text-center md:tracking-wide mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi I&apos;m Muhammed Shameer, a web developer based in India
+            </p>
           </div>
         </div>
       </div>
