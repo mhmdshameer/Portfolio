7fda26dda91c104e5b4281b9817fd5582e70aa8f
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-07-05 11:01:16 +0530

    add image for 1 and 6

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
     <div
       className={cn(
         "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
-        className,
+        className
       )}
     >
       {children}
@@ -42,7 +42,7 @@ export const BentoGridItem = ({
     <div
       className={cn(
         "group/bento shadow-input row-span-1 flex flex-col relative justify-between space-y-4 rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
-        className,
+        className
       )}
       style={{
         background: "rgb(4,7,29)",
@@ -50,10 +50,19 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="">
-          {title}
+      <div className={`${id === 6} && flex justify-center h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+            />
+          )}
         </div>
+      </div>
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
+        <div className="">{title}</div>
         <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
           {description}
         </div>
